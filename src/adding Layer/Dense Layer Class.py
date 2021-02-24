58e2e920b720943bf641214c0f8c26489c98297a
Abdul Motin <motin.mmk.jr@gmail.com>
2021-02-24 09:45:05 +0600

    layer_densec

---
+++ src/adding Layer/Dense Layer Class.py
@@ -0,0 +1,23 @@
+# Now that we no longer need to hand-type our data, we should create something similar for our
+# various types of neural network layers. So far, we’ve only used what’s called a dense or
+# fully-connected layer. These layers are commonly referred to as “dense” layers in papers,
+# literature, and code, but you will occasionally see them called fully-connected or “fc” for short in
+# code. Our dense layer class will begin with two methods.
+
+
+# Otimitions
+# As previously stated, weights are often initialized randomly for a model, but not always. If you
+# wish to load a pre-trained model, you will initialize the parameters to whatever that pretrained
+# model finished with. It’s also possible that, even for a new model, you have some other
+# initialization rules besides random. For now, we’ll stick with random initialization. Next, we have
+# the forward method. When we pass data through a model from beginning to end, this is called a
+# forward pass . Just like everything else, however, this is not the only way to do things. You can
+# have the data loop back around and do other interesting things. We’ll keep it usual and perform a
+# regular forward pass.
+
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        pass
+
+    def forword(self, inputs):
+        pass
