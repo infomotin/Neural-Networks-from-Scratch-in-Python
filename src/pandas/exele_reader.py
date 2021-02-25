f50e2370852b4eee5f2e6fb87ab3c283cf6cd3e6
Abdul Motin <motin.mmk.jr@gmail.com>
2021-02-25 16:42:08 +0600

    Layer_Dense

--- src/pandas/exele_reader.py
+++ src/pandas/exele_reader.py
@@ -1,4 +1,12 @@
 import pandas as pd
 
 df = pd.read_csv('./src/pandas/data.csv')
-print(df)
+# print(df.shape)
+# row, col = df.shape
+# print(row, ' ', col)
+# print(df[5:80])
+
+# print(df.columns)
+# print(df["EMP_NAME"])
+# print(df.EMP_NAME)
+print(df.head())
